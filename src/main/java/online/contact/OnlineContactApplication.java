7c33d3d07b99f377198367665e7304e4069aa977
fajaragungpramana <fajar.agungpramana01@gmail.com>
2022-01-29 19:46:04 +0700

    initial commit.

---
+++ src/main/java/online/contact/OnlineContactApplication.java
@@ -0,0 +1,13 @@
+package online.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineContactApplication.class, args);
+	}
+
+}
