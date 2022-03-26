79ead1a2614615c6f488f5f02eff9e957db4eb86
fajaragungpramana <fajar.agungpramana01@gmail.com>
2022-03-26 11:36:13 +0700

    Add new route.

--- src/main/java/online/contact/model/constant/HttpRoute.java
+++ src/main/java/online/contact/model/constant/HttpRoute.java
@@ -3,5 +3,6 @@ package online.contact.model.constant;
 public class HttpRoute {
 
     public static final String VERSION = "/version";
+    public static final String USER_REGISTER = "/user/register";
 
 }
